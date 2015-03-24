5d7ba9f1783201df973a7c69b3f887305a28861e
Chance <chancerh@gmail.com>
2015-03-23 21:54:51 -0700

    Initial commit

---
+++ movies.py
@@ -0,0 +1,13 @@
+import webbrowser
+class Movie():
+    def __init__(self, title, synopsis, poster_image_url, trailer_youtube_url):
+        self.title = title
+        self.synopsis = synopsis
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailerUrl)
+
+
+
