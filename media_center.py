ac278f523005341112c75cd298854a037436b100
Chance <chancerh@gmail.com>
2015-03-23 22:30:03 -0700

    Clean up comments

--- media_center.py
+++ media_center.py
@@ -4,35 +4,42 @@ import fresh_tomatoes
 # Fallen Angels
 fallen_angels = Movie("Fallen Angels",
                       "Two stories in Hong Kong with a few casual run-ins.",
-                      "http://upload.wikimedia.org/wikipedia/en/0/04/Fallen-Angels-Poster.jpg",
+                      "http://upload.wikimedia.org/wikipedia/en/0/04/Fallen-"
+                      "Angels-Poster.jpg",
                       "https://www.youtube.com/watch?v=-NfjlmxuCrU")
 
 # Akira
 akira = Movie("Akira",
               "Neo-Tokyo is about to explode.",
-              "http://upload.wikimedia.org/wikipedia/en/5/5d/AKIRA_%281988_poster%29.jpg",
+              "http://upload.wikimedia.org/wikipedia/en/5/5d/AKIRA_%281988_"
+              "poster%29.jpg",
               "https://www.youtube.com/watch?v=-UhLderbuGI")
 
 # Amelie
 cityLostChildren = Movie("City of Lost Children",
                          "90s French Steampunk",
-                         "http://upload.wikimedia.org/wikipedia/en/4/40/City_of_lost_children_french_movie_poster.jpg",
+                         "http://upload.wikimedia.org/wikipedia/en/4/40/City_"
+                         "of_lost_children_french_movie_poster.jpg",
                          "https://www.youtube.com/watch?v=CNYG9cXTSds",)
 
 
 # Run Lola Run
 lolaRennt = Movie("Run Lola Run",
                   "A study of how small changes can create different outcomes",
-                  "http://upload.wikimedia.org/wikipedia/en/f/f5/Lola_Rennt_poster.jpg",
+                  "http://upload.wikimedia.org/wikipedia/en/f/f5/Lola_Rennt_"
+                  "poster.jpg",
                   "https://youtu.be/LbYk77LjDtQ")
 
 # Night on earth
 night_on_earth = Movie("Night On Earth",
                        "5 Stories, 5 Taxi Drivers, 5 Locations, One Night",
-                       "http://upload.wikimedia.org/wikipedia/en/f/f9/Nightonearth.jpg",
+                       "http://upload.wikimedia.org/wikipedia/en/f/f9/"
+                       "Nightonearth.jpg",
                        "https://www.youtube.com/watch?v=XkKBbibm1FM")
 
 
+# Store movies in a list
 movieList = [fallen_angels, akira, cityLostChildren, lolaRennt, night_on_earth]
 
+# Render and launch movies website.
 fresh_tomatoes.open_movies_page(movieList)
--- movies.py
+++ movies.py
@@ -2,6 +2,7 @@ import webbrowser
 
 
 class Movie():
+    # Define a class to store movie information and display trailer
     def __init__(self, title, synopsis, poster_image_url, trailer_youtube_url):
         self.title = title
         self.synopsis = synopsis
@@ -9,4 +10,5 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
+        # displays movie trailer
         webbrowser.open(self.trailerUrl)
